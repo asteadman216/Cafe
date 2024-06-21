181a5dbb3cd57b8a8c0712c2b4a59da0c8894def
Varun Sudharsanam <49537404+ksvarunbabu@users.noreply.github.com>
2024-06-20 20:36:30 -0500

    Update settings.py
    Added static files path and root...

--- Cafe/settings.py
+++ Cafe/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import os
+
+MEDIA_URL = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
