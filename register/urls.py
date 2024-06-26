8c06f30b86e6da303e0b9edc8cb55375fa21ec29
Varun KS <ksvarun@gmail.com>
2024-06-26 17:53:09 -0500

    fixed redirecting register user to login after registration

--- register/urls.py
+++ register/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.register, name='register'),
+    path('register/', views.register, name='register'),
 ]
--- register/views.py
+++ register/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth.models import User, Group
 
+
 def register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
@@ -12,10 +13,10 @@ def register(request):
             lib_group = Group.objects.get(name='customer')
             user.groups.add(lib_group)
             user.save()
-            return redirect('home')  # Redirect to 'home' after successful registration
+            return redirect('login')  # Redirect to login after successful registration
         else:
             # If form is not valid, render the registration form with errors
-            return render(request, "register.html", {"form": form})
+            return redirect("home")
     else:
         form = RegisterForm()
     return render(request, "register.html", {"form": form})
